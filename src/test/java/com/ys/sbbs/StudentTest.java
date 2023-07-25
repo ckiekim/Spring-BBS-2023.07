20fe551c2d71b60099b88b07db6f7e81a2b1ae71
Cheolkie Kim <ckiekim@naver.com>
2023-07-25 15:06:10 +0900

    Spring Boot

---
+++ src/test/java/com/ys/sbbs/StudentTest.java
@@ -0,0 +1,14 @@
+package com.ys.sbbs;
+
+import com.ys.sbbs.entity.Student;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+
+		Student st = new Student();
+		//st.setId(58);
+		
+	}
+
+}
