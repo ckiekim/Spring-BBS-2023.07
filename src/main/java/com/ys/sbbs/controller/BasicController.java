48c279d0843b03a153adbcaf1c8f9eece1818939
Cheolkie Kim <ckiekim@naver.com>
2023-07-25 12:14:35 +0900

    Spring Boot

---
+++ src/main/java/com/ys/sbbs/controller/BasicController.java
@@ -0,0 +1,18 @@
+package com.ys.sbbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+	// localhost:8080/sbbs/basic/basic1
+	@RequestMapping("/basic1")
+	public String basic1() {
+		// application.properties에 prefix=/WEB-INF/view/, suffix=.jsp
+		// /WEB-INF/view/basic/basic1.jsp
+		return "basic/basic1";
+	}
+	
+}
