5aea89fc8a64707e583591199da17f2a65b80799
Cheolkie Kim <ckiekim@naver.com>
2023-08-03 08:42:06 +0900

    Day 6

--- src/main/java/com/ys/sbbs/filter/SbbsFilter.java
+++ src/main/java/com/ys/sbbs/filter/SbbsFilter.java
@@ -31,11 +31,14 @@ public class SbbsFilter extends HttpFilter implements Filter {
 			session.setAttribute("menu", "board");
 		else if (uri.contains("user"))
 			session.setAttribute("menu", "user");
+		else if (uri.contains("schedule"))
+			session.setAttribute("menu", "schedule");
 		else
 			session.setAttribute("menu", "");
 		
+		
 		String sessionUid = (String) session.getAttribute("sessUid");
-		String[] urlPatterns = { "/board", "/aside", "/file", 
+		String[] urlPatterns = { "/board", "/aside", "/file", "/schedule",
 								 "/user/list", "/user/update", "/user/delete" };
 		for (String routing: urlPatterns) {
 			if (uri.contains(routing)) {
